d4024a685659c042124b1dd513663a6174412925
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-03-28 16:25:19 +0800

    Material app演示

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,127 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-void main() => runApp(new MyApp());
+//gxw- for study flutter  void main() => runApp(new MyApp());
 
+
+/**
+ * runApp(根widget)
+ * 包括Center和Text两个Widget
+ */
+void main() {
+  runApp(new MaterialApp(
+    title: 'My app', // used by the OS task switcher
+    //home: new MyScaffold(),
+    home: new TutorialHome()
+  ));
+}
+
+
+
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Scaffold是Material中主要的布局组件.
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: new Text('Example title'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+
+        ],
+      ),
+      //body占屏幕的大部分
+      body: new Center(
+        child: new Text('Hello, world!'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: new Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Widget子类中的字段往往都会定义为"final"
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 56.0, // 单位是逻辑上的像素（并非真实的像素，类似于浏览器中的像素）
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: new BoxDecoration(color: Colors.blue[500]),
+      // Row 是水平方向的线性布局（linear layout）
+      child: new Row(
+        //列表项的类型是 <Widget>
+        children: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null 会禁用 button
+          ),
+          // Expanded expands its child to fill the available space.
+          new Expanded(
+            child: title,
+          ),
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Material 是UI呈现的“一张纸”
+    return new Material(
+      // Column is 垂直方向的线性布局.
+      child: new Column(
+        children: <Widget>[
+          new MyAppBar(
+            title: new Text(  //传递一个Text Widget
+              'my title',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          new Expanded(
+            child: new Center(
+              child: new Text('Hello, world!'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+//收藏列表
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
