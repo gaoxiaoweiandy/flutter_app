181c1672053eb67ef130dba03f7411ff8284db1b
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-03 09:56:10 +0800

    基础列表

--- lib/main.dart
+++ lib/main.dart
@@ -19,25 +19,85 @@ void main() {
   t.b();*/
 
 
-  runApp(new MaterialApp(
-    title: 'My app', // used by the OS task switcher
-    home: new RaiseButtonDemo(),
-
-    //home: new MyScaffold(),
-    //gxw2-home: new Counter()//单击增加计数
- /*   home: new ShoppingList(
-      products: <Product>[
-        new Product(name: 'Eggs'),
-        new Product(name: 'Flour'),
-        new Product(name: 'Chocolate chips'),
-      ],
-    )*/
-  ));
+  runApp(new MyApp());
 }
 
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final title = "基础列表示例";
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        //添加基础列表
+        body: new ListView(
+          //添加静态数据
+          children: <Widget>[
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+            ListTile(
+              leading: Icon(Icons.airplay),
+              title: Text('Airplay'),
+            ),
+            ListTile(
+              leading: Icon(Icons.airplay),
+              title: Text('Airplay'),
+            ),
+            ListTile(
+              leading: Icon(Icons.airplay),
+              title: Text('Airplay'),
+            ),
+            ListTile(
+              leading: Icon(Icons.airplay),
+              title: Text('Airplay'),
+            ),
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text('Alarm'),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+
+
 class RaiseButtonDemo extends StatelessWidget {
-
-
 
 //iconButton
   @override
@@ -762,6 +822,7 @@ class MyScaffold extends StatelessWidget {
 
 
 
+/*
 //收藏列表
 class MyApp extends StatelessWidget {
   @override
@@ -776,6 +837,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
 
 class RandomWords extends StatefulWidget
 {
