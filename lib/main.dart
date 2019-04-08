4a0921918721b97d164393ef75cde3f8931f497b
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-08 14:37:15 +0800

    Material design App

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,49 @@ void main() {
   t.b();*/
 
 
-  runApp(new LoginPage(
+  runApp(new MyMeterialDesignApp(
 
   ));
 }
 
+
+//Material design APP START
+class MyMeterialDesignApp extends StatelessWidget {
+  //这是整个应用的主组件
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new MyHomePage(),
+      title: 'MaterialApp示例',
+    );
+  }
+}
+
+//这是一个可改变的Widget
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('MaterialApp示例'),
+      ),
+      body: Center(
+        child: Text(
+          '主页',
+          style: TextStyle(fontSize: 28.0),
+        ),
+      ),
+    );
+  }
+}
+
+
+//Material design APP END
 
 
 //表单START
@@ -431,7 +469,7 @@ class MyAppForUsePackage extends StatelessWidget {
 
 
 //主题应用START
-class MyAppForThem extends StatelessWidget {
+/*class MyAppForThem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appName = '自定义主题';
@@ -481,7 +519,7 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
-}
+}*/
 //主题应用END
 
 
