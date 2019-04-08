b0556b56b166719dca244a57fcf555e507f26deb
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-08 15:55:35 +0800

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,59 @@ import 'dart:io';
 void main() => runApp(
   new MaterialApp(
     title: 'Scaffold脚手架组件示例',
-    home: new LayoutDemoForAppbar(),
+    home: new MyHomePage(),
   ),
 );
+
+
+//BottomNavigationBar Demo Start
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1;//当前选中项的索引
+  final _widgetOptions = [
+    Text('Index 0: 信息'),
+    Text('Index 1: 通讯录'),
+    Text('Index 2: 发现'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BottomNavigationBar示例'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),//居中显示某一个文本
+      ),
+      //底部导航按钮 包含图标及文本
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text('信息')),
+          BottomNavigationBarItem(icon: Icon(Icons.contacts), title: Text('通讯录')),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('发现')),
+        ],
+        currentIndex: _selectedIndex,//当前选中项的索引
+        fixedColor: Colors.deepPurple,//选项中项的颜色
+        onTap: _onItemTapped,//选择按下处理
+      ),
+    );
+  }
+
+  //选择按下处理 设置当前索引为index值
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+//BottomNavigationBar Demo End
+
 
 
 //AppBar demo start
@@ -86,7 +136,7 @@ class LayoutDemoForScaffold extends StatelessWidget {
 
 
 
-class MyAppForRoute extends StatelessWidget {
+/*class MyAppForRoute extends StatelessWidget {
   //这是整个应用的主组件
   @override
   Widget build(BuildContext context) {
@@ -125,7 +175,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
+}*/
 
 //第一个路由页面
 class FirstPage extends StatelessWidget {
