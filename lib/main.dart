31b63eed91433f66065af8c68a4691e4063af4c4
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-09 17:28:04 +0800

    AlertDlg

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,54 @@ import 'dart:io';
 void main() => runApp(
   new MaterialApp(
     title: 'Scaffold脚手架组件示例',
-    home: new MyAppForSimpleDlg(),
+    home: new MyAppForAlertDlg(),
   ),
 );
+
+
+//MyAppForAlertDlg START
+class MyAppForAlertDlg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PopupMenuButton组件示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('PopupMenuButton组件示例'),
+        ),
+        body: Center(
+          child: AlertDialog(
+            title: Text('提示'), //对话框标题
+            content: SingleChildScrollView(
+//对话框内容部分
+              child: ListBody(
+                children: <Widget>[
+                  Text('是否要删除?'),
+                  Text('一旦删除数据不可恢复！'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('确定'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('取消'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+//MyAppForAlertDlg END
+
+
+
+
 
 
 //MyAppForSimpleDlg
