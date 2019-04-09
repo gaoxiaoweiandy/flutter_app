dbdcfbfbd930c9818fbb46b7d9d25e4881d0bc88
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-09 17:00:11 +0800

    FlatButton与FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,61 @@ import 'dart:io';
 void main() => runApp(
   new MaterialApp(
     title: 'Scaffold脚手架组件示例',
-    home: new LayoutDemoDrawer(),
+    home: new MyAppFloatButton(),
   ),
 );
+class MyAppFloatButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FloatingActionButton示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FloatingActionButton示例'),
+        ),
+        body: Center(
 
+       child:    FlatButton(
+            onPressed: () {},
+            child: Text(
+              'FlatButton',
+              style: TextStyle(fontSize: 24.0,color: Colors.red
+              ),
+            ),
+          ),
+
+        ),
+        floatingActionButton: new Builder(builder: (BuildContext context) {
+          return new FloatingActionButton(
+            child: const Icon(Icons.add),
+            //提示信息
+            tooltip: "请点击FloatingActionButton",
+            //前景色为白色
+            foregroundColor: Colors.white,
+            //背景色为蓝色
+            backgroundColor: Colors.blue,
+            //未点击阴影值
+            elevation: 7.0,
+            //点击阴影值
+            highlightElevation: 14.0,
+            onPressed: () {
+              //点击回调事件 弹出一句提示语句
+              Scaffold.of(context).showSnackBar(new SnackBar(
+                content: new Text("你点击了FloatingActionButton"),
+              ));
+            },
+            mini: true,
+            //圆形边
+            shape: new CircleBorder(),
+            isExtended: true,
+          );
+        }),
+        floatingActionButtonLocation:
+        FloatingActionButtonLocation.centerFloat, //居中放置 位置可以设置成左中右
+      ),
+    );
+  }
+}
 
 //drawer start
 class LayoutDemoDrawer extends StatelessWidget {
