563eee9c0ac60bc29976c6829f8e0e49324f34f4
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-10 10:41:04 +0800

    Padding

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,56 @@ void main() => runApp(
 
      new MaterialApp(
        title:"CENTER 演示",
-       home:new LayoutDemoForCenter(),
+       home:new LayoutDemoForPadding(),
 )
 
-
 );
+
+
+
+
+class LayoutDemoForPadding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Padding填充布局示例'),
+      ),
+      body: new Center(
+
+        child: new Container(
+          width: 300.0,
+          height: 300.0,
+          padding: EdgeInsets.all(6.0),//容器填充上下左右设置为60.0
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            border: new Border.all(
+              color: Colors.green,
+              width: 8.0,
+            ),
+          ),
+          child: new Container(
+            width: 200.0,
+            height: 200.0,
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              border: new Border.all(
+                color: Colors.blue,
+                width: 8.0,
+              ),
+            ),
+            child: new FlutterLogo(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
 
 //Center Widget START
 class LayoutDemoForCenter extends StatelessWidget {
