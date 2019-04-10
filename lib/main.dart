0f47915e7b33adfb22d44fd3561a1b425b63b575
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-10 11:29:02 +0800

    Row:用于水平布局

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,43 @@ void main() => runApp(
 
      new MaterialApp(
        title:"rAlign 演示",
-       home:new LayoutDemoForAlign(),
+       home:new LayoutDemoForRow(),
 )
 
 );
+
+
+
+class LayoutDemoForRow extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('水平布局示例'),
+      ),
+      body: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Text('左侧文本', textAlign: TextAlign.center),
+            flex: 4,
+          ),
+          new Expanded(
+            child: new Text('中间文本', textAlign: TextAlign.center),
+          ),
+          new Expanded(
+            child: new FittedBox(
+              fit: BoxFit.contain,
+              child: const FlutterLogo(),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
 
 //ALIGN START
 class LayoutDemoForAlign extends StatelessWidget {
