501110b8e013cf8a38c1a5f3e64daac2bd8a6b36
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-04-11 14:29:34 +0800

    //SizeBox,如何指定了宽高，则SizeBox就是这个宽高，如果没有指定，SizeBox随child的宽高而变化

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,33 @@ import 'dart:io';
 void main() => runApp(
 
      new MaterialApp(
-       home:new LayoutDemoForOverFlowBox(),
+       home:new LayoutDemoForSizeBox(),
 )
 
 );
 
+//SizeBox,如何指定了宽高，则SizeBox就是这个宽高，如果没有指定，SizeBox随child的宽高而变化
+class LayoutDemoForSizeBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('SizedBox设置具体尺寸示例'),
+      ),
+      body: SizedBox(
+        //固定宽为200.0 高为300.0
+        width: 200.0,
+        height: 300.0,
+        child: const Card(
+            child: Text('SizedBox',
+              style: TextStyle(
+                fontSize: 36.0,
+              ),
+            )),
+      ),
+    );
+  }
+}
 
 
 class LayoutDemoForOverFlowBox extends StatelessWidget {
